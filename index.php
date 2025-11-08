5c2eea5fa9445a54ea70f307cdb3b66fee4e70ec
Isma-ae <36768882+Isma-ae@users.noreply.github.com>
2025-11-08 14:03:20 +0700

    navbar

--- index.php
+++ index.php
@@ -10,8 +10,33 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">ระบบประปาหมู่บ้าน</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" href="#">หน้าหลัก</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">ผู้ใช้น้ำ</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">บันทึกการใช้น้ำ</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">รายงาน</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container">
-        <!-- เนื้อหาเว็บไซต์ -->
+        <div class="row">
+
+            <div class="col-sm-12 col-md-6 col-lg-6">คอลัมน์ 1</div>
+
+            <div class="col-sm-12 col-md-6 col-lg-6">คอลัมน์ 2</div>
+
+            <div class="col-sm-12 col-md-12 col-lg-6">คอลัมน์ 3</div>
+            <div class="col-sm-12 col-md-12 col-lg-6">คอลัมน์ 3</div>
+
+        </div>
     </div>
 
     <!-- Bootstrap JS -->
