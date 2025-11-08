7ea8362e6d2113a1d5a16720f189bc1f52741cb1
Isma-ae <36768882+Isma-ae@users.noreply.github.com>
2025-11-08 13:53:01 +0700

    ิbootstrap

--- index.php
+++ index.php
@@ -1,39 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="th">
 
 <head>
-    <title>Bootstrap Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ระบบประปาหมู่บ้าน</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-sm bg-primary text-light">
-        <div class="container-fluid">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: white">เพิ่มสมาชิก555</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: white">เพิ่มข้อมูลหน่วยที่ใช้</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style="color: white">ออกจากระบบ</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <div class="container-fluid mt-3">
-        <h3>ระบบประปา</h3>
-        <p>555</p>
-        <p>The navbar-expand-xxl|xl|lg|md|sm class determines when the navbar should stack vertically (on xxlarge, extra
-            large, large, medium or small screens).</p>
+    <div class="container">
+        <!-- เนื้อหาเว็บไซต์ -->
     </div>
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
