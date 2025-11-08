816093f1c678399edb30bd443a6f41eaf5999584
Isma-ae <36768882+Isma-ae@users.noreply.github.com>
2025-11-08 14:19:33 +0700

    dashbord

--- index.php
+++ index.php
@@ -29,12 +29,24 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-sm-12 col-md-6 col-lg-6">คอลัมน์ 1</div>
+            <div class="col-sm-12 col-md-6 col-lg-3 text-center">
+                <h1>287</h1>
+                <h4>ผู้ใช้น้ำทั้งหมด<h4>
+            </div>
 
-            <div class="col-sm-12 col-md-6 col-lg-6">คอลัมน์ 2</div>
+            <div class="col-sm-12 col-md-6 col-lg-3 text-center">
+                <h1>15,420</h1>
+                <h4>ค่าน้ำค้างชำระ<h4>
+            </div>
 
-            <div class="col-sm-12 col-md-12 col-lg-6">คอลัมน์ 3</div>
-            <div class="col-sm-12 col-md-12 col-lg-6">คอลัมน์ 3</div>
+            <div class="col-sm-12 col-md-12 col-lg-3 text-center">
+                <h1>75%</h1>
+                <h4>ปริมาณน้ำในถัง<h4>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-3 text-center">
+                <h1>3</h1>
+                <h4>มิเตอร์ที่ชำรุด<h4>
+            </div>
 
         </div>
     </div>
